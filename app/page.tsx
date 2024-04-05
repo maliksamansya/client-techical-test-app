843fd56aff44a5607566674889239c4ff6b9a718
Malik Samansya <maliksamansya206@gmail.com>
2024-04-05 10:56:37 +0700

    feat: config

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,233 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
+import {
+  Button,
+  Flex,
+  Input,
+  Space,
+  Table,
+  Tag,
+  TableProps,
+  GetProp,
+} from "antd";
+import type { SearchProps } from "antd/es/input/Search";
+import { AudioOutlined } from "@ant-design/icons";
+import qs from "qs";
+import { useEffect, useState } from "react";
+const { Search } = Input;
 
+const suffix = (
+  <AudioOutlined
+    style={{
+      fontSize: 16,
+      color: "#1677ff",
+    }}
+  />
+);
+
+type ColumnsType<T> = TableProps<T>["columns"];
+type TablePaginationConfig = Exclude<
+  GetProp<TableProps, "pagination">,
+  boolean
+>;
+
+interface DataType {
+  name: string;
+  age: number;
+  location: string;
+  email: string;
+  phone: string;
+  cell: string;
+  picture: string;
+  // login: {
+  //   uuid: string;
+  // };
+}
+
+interface TableParams {
+  pagination?: TablePaginationConfig;
+  sortField?: string;
+  sortOrder?: string;
+  filters?: Parameters<GetProp<TableProps, "onChange">>[1];
+}
+
+const columns: ColumnsType<DataType> = [
+  {
+    title: "Nama",
+    dataIndex: "name",
+    width: "20%",
+  },
+  {
+    title: "Umur",
+    dataIndex: "age",
+    width: "20%",
+  },
+  {
+    title: "Alamat",
+    dataIndex: "location",
+  },
+  {
+    title: "Email",
+    dataIndex: "email",
+  },
+  {
+    title: "No. Telepon 1",
+    dataIndex: "phone",
+  },
+  {
+    title: "No. Telepon 2",
+    dataIndex: "cell",
+  },
+  {
+    title: "Gambar",
+    dataIndex: "picture", // this is the value that is parsed from the DB / server side
+    key: "picture",
+    render: (pictureArray) => (
+      <Image
+        src={pictureArray[0]}
+        alt="User"
+        width={250}
+        height={100}
+        style={{ borderRadius: "2px" }}
+      />
+    ),
+  },
+
+  // {
+  //   title: "Gambar",
+  //   dataIndex: "picture", // this is the value that is parsed from the DB / server side
+  //   key: 'picture',
+  //   render: (theImageURL) => (
+  //     <>
+  //       {theImageURL.map((tag: any) => {
+  //         // let color = tag.length > 5 ? "geekblue" : "green";
+  //         // if (tag === "loser") {
+  //         //   color = "volcano";
+  //         // }
+  //         return (
+  //           <Image key={tag} alt={tag} src={tag} width={250} height={250} />
+  //         );
+  //       })}
+  //     </>
+  //   ), // 'theImageURL' is the variable you must declare in order the render the URL
+  // },
+  //     title: "Tags",
+  //     key: "tags",
+  //     dataIndex: "tags",
+  //     render: (_, { tags }) => (
+  //       <>
+  //         {tags.map((tag) => {
+  //           let color = tag.length > 5 ? "geekblue" : "green";
+  //           if (tag === "loser") {
+  //             color = "volcano";
+  //           }
+  //           return (
+  //             <Tag color={color} key={tag}>
+  //               {tag.toUpperCase()}
+  //             </Tag>
+  //           );
+  //         })}
+  //       </>
+  //     ),
+  //   },
+];
+
+const getRandomuserParams = (params: TableParams) => ({
+  results: params.pagination?.pageSize,
+  page: params.pagination?.current,
+  ...params,
+});
 export default function Home() {
+  const [data, setData] = useState<DataType[]>();
+  const [loading, setLoading] = useState(false);
+  const [name, setName] = useState<string>("");
+  const [tableParams, setTableParams] = useState<TableParams>({
+    pagination: {
+      current: 1,
+      pageSize: 10,
+    },
+  });
+  const onSearch: SearchProps["onSearch"] = (value, _e, info) => {
+    // console.log(info?.source, value);
+    setName(value);
+  };
+  console.log(name, "<<<< name");
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(
+        `http://localhost:3001/manipulate-api?${qs.stringify(
+          getRandomuserParams(tableParams)
+        )}&name=${name}`
+      );
+      const data = await response.json();
+      setData(data);
+      setLoading(false);
+      setTableParams({
+        ...tableParams,
+        pagination: {
+          ...tableParams.pagination,
+          total: 200,
+          // 200 is mock data, you should read it from server
+          // total: data.totalCount,
+        },
+      });
+    } catch (error) {
+      // Handle errors here
+      console.error("Error fetching data:", error);
+      setLoading(false); // Make sure loading state is set to false
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [JSON.stringify(tableParams), name]);
+
+  const handleTableChange: TableProps["onChange"] = (
+    pagination,
+    filters,
+    sorter
+  ) => {
+    setTableParams({
+      pagination,
+      filters,
+      ...sorter,
+    });
+
+    // `dataSource` is useless since `pageSize` changed
+    if (pagination.pageSize !== tableParams.pagination?.pageSize) {
+      setData([]);
+    }
+  };
+  const boxStyle: React.CSSProperties = {
+    width: "100%",
+    // height: 120,
+    borderRadius: 6,
+    // border: "1px solid #40a9ff",
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <div className={styles.container}>
+      <Space direction="vertical" style={{ width: "100%" }}>
+        <Flex style={boxStyle} justify={"space-between"} align={"center"}>
+          <Search
+            placeholder="input search text"
+            onSearch={onSearch}
+            style={{ width: 200 }}
+          />
+          <Button>New Data</Button>
+        </Flex>
+        {/* <Table columns={columns} dataSource={data} /> */}
+        <Table
+          columns={columns}
+          // rowKey={(record) => record.login.uuid}
+          dataSource={data}
+          pagination={tableParams.pagination}
+          loading={loading}
+          onChange={handleTableChange}
         />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      </Space>
+    </div>
   );
 }
